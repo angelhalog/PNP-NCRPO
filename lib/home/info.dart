abd22da545aa483e868bc6eeaa29929a8865f98e
Angel Halog <halogjanssenangel@gmail.com>
2024-11-13 15:19:42 +0800

    updated

--- lib/home/info.dart
+++ lib/home/info.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class InfoScreen extends StatelessWidget {
@@ -14,8 +16,40 @@ class InfoScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Text('This is the app information screen.'),
-      ),
+        child: Container(
+          color: Colors.white.withOpacity(0.5),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/rdNCRPO.png',
+                  width: 450, 
+                  height: 250, 
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 1),
+
+                Image.asset(
+                  'assets/images/infoText.png',
+                  width: 320,
+                  height: 170,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 3),
+
+                Image.asset(
+                  'asset/images/92.png',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.contain,
+                ),
+                const SizedBox(height: 3),
+              ]
+            ),
+          )
+        )
+      )
     );
   }
 }
